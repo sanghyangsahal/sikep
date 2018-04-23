37b2ee1a662a26b441fa9afa9e1784efba3df0b5
sanghyangsahal <sanghiangsahal40@gmail.com>
2018-04-24 00:06:20 +0700

    Penambahan Komponen Form

--- sikep/backend/views/layouts/left.php
+++ sikep/backend/views/layouts/left.php
@@ -8,7 +8,7 @@
                 <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p><?= Yii::$app->user->identity->username ?></p>
+                <p></p>
 
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
@@ -46,15 +46,7 @@
                             ['label' => 'Diklat Kualifikasi Kelulusan', 'icon' => 'user', 'url' => ['/refdiklatkualifikasikelulusan/index'],],
                         ],
                     ],
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                        ],
-                    ],
+                    ['label' => 'Komponen', 'icon' => 'file-code-o', 'url' => ['/komponen/index']],
                 ],
             ]
         ) ?>
